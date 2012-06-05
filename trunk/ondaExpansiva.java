6e1e07aff590a5f31b36fee5aa1eea2316b84b65
francisco.disalvo90 <francisco.disalvo90@63393462-3f79-a54c-fcff-b23df89fc2d5>
2012-06-05 18:07:50 +0000

    Es la interfaz de la clase onda expansiva, para la cual faltan cosas porq nose que hay en la clase bloque ,tiene como atributo el radio de la explosion, y como metodos el constructor, un getRadio, otro metodo para identificar la zona donde se esparce la onda, y por ultimo un metodo que debilita los bloque que se encuentra dentro de esa onda.

---
+++ trunk/ondaExpansiva.java
@@ -0,0 +1,23 @@
+import claseBloque;
+
+class ondaExpansiva {
+     int radio;
+     
+     public void OndaExpansiva(int r){//Constructor.
+    	 radio=r;
+     }
+     
+     public int getRadio(){
+    	 return radio;
+     }
+     
+     public void identificarZona(int x,int y){/*A partir de la posicion donde explota la bomba,
+                                             identifica la zona afectada de acuerdo al radio*/
+    	 
+     }
+     
+     public void degradarBloque(Bloque B){//Si encuentra un bloque lo degrada.
+    	 
+     }
+     
+}
