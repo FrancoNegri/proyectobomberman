be46210069878fc264f4297e221a6a4c02ff177a
francisco.disalvo90 <francisco.disalvo90@63393462-3f79-a54c-fcff-b23df89fc2d5>
2012-06-12 05:12:59 +0000

    Algunos cambios en la onda expansiva

--- BombitaTp2/src/modelo/ondaexpansiva/OndaExpansiva.java
+++ BombitaTp2/src/modelo/ondaexpansiva/OndaExpansiva.java
@@ -1,25 +1,49 @@
 package modelo.ondaexpansiva;
-
 import modelo.obstaculos.Obstaculo;
+import modelo.personaje.Caminable;
+import modelo.personaje.Personaje;
+import modelo.personaje.movimiento.*;
 
-public class OndaExpansiva {
+public class OndaExpansiva implements Caminable {
     private int radio;
+    private int danio;
+	private Movimiento Arriba;
     
-    public OndaExpansiva(int r) {
-   	 this.radio=r;
+    public OndaExpansiva(int r,int d) {
+   	 	this.radio=r;
+   	 	this.danio=d;
     }
     
     public int getRadio(){
-   	 return radio;
+   	 	return radio;
     }
     
-    public void caminarZona(int x,int y){/*A partir de la posicion donde explota la bomba,
-                                            camina como un objeto la zona afectada de acuerdo
-                                            al radio*/
-   	 
+    public int getDanio(){
+    	return danio;
+    }
+    
+    public static void Mover(Personaje unPersonaje){
+    	
+    }
+    
+    public void Expandirse(int x,int y){//A partir de la posicion donde explota la bomba,camina como un objeto la zona afectada de acuerdo al radio
+    	int i,j;
+    	Personaje k = null;
+    	
+    	for(i=1;i<5;i++)
+    		for(j=1;j<=this.radio;j++){
+    			OndaExpansiva.Mover(k); //En proceso.
+    		}
     }
     
     public void degradarBloque(Obstaculo unObstaculo){//Si encuentra un bloque lo degrada.
-   	 
+   	   	unObstaculo.QuitarVida(this.danio);    
+    	
     }
+
+	@Override
+	public void mover(Personaje unPersonaje) {
+		// TODO Auto-generated method stub
+		
+	}
 }
