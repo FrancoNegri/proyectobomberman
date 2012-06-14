81057f4540be3fcfd5d968c9157809abd406bef2
fede.dirocco@gmail.com <fede.dirocco@gmail.com@63393462-3f79-a54c-fcff-b23df89fc2d5>
2012-06-14 04:08:42 +0000

    Borre clase coordinadormapa e inclui los metodos en la clase mapa.

--- BombitaTp2/src/modelo/mapa/Mapa.java
+++ BombitaTp2/src/modelo/mapa/Mapa.java
@@ -2,7 +2,10 @@ package modelo.mapa;
 
 
 
+import modelo.articulo.Articulable;
 import modelo.casillero.Casillero;
+import modelo.obstaculos.Obstaculo;
+import modelo.personaje.Personaje;
 
 public class Mapa {
 	
@@ -26,5 +29,22 @@ public class Mapa {
 		
 	}
 	
+	public void modificarCasillero(int x, int y, Casillero unCasillero, Mapa unMapa ){
+		if (unCasillero.estaVacio()) {
+			unMapa.agregarCasillero(x, y, unCasillero);
+		//Agregar excepcion para ver cuando el casillero no esta vacio
+		}
+	}
 	
+	public void agregarPersonaje(Personaje unPersonaje, Mapa unMapa ){
+		
+	}
+
+	public void agregarArticulo(int x, int y, Articulable unArticulo, Mapa unMapa ){
+		
+	}
+	
+	public void agregarBloque(int x, int y, Obstaculo unObstaculo, Mapa unMapa){
+		
+	}
 }
