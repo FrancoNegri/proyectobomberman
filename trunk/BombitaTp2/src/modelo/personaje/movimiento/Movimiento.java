50a54634c9691610cf3b8a630701d8b4e4417641
fede.dirocco@gmail.com <fede.dirocco@gmail.com@63393462-3f79-a54c-fcff-b23df89fc2d5>
2012-06-15 19:50:15 +0000

    Modificando la clase movimiento con la clase translacion

--- trunk/BombitaTp2/src/modelo/personaje/movimiento/Movimiento.java
+++ trunk/BombitaTp2/src/modelo/personaje/movimiento/Movimiento.java
@@ -1,39 +1,41 @@
 package modelo.personaje.movimiento;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 
+import modelo.coordenadas.Translacion;
 import modelo.personaje.Caminable;
 import modelo.personaje.Personaje;
 
 public abstract class Movimiento implements Caminable {
-	
-		//private ArrayList<Movimiento> movimientos;
-		//private Derecha derecha;
-		//private Izquierda izquierda;
-		//private Arriba arriba;
-		//private Abajo abajo;
-	
-	public Movimiento(){
-		  //this.movimientos = new ArrayList<Movimiento>();
-		
-		  //derecha = new Derecha();
-		  //izquierda = new Izquierda();
-		  //abajo = new Abajo();
-		  //arriba = new Arriba();
-		
-		  //Inicializo la lista de objetos movimientos
-		  //this.movimientos.add(derecha);
-		  //this.movimientos.add(izquierda);
-		  //this.movimientos.add(abajo);
-		  //this.movimientos.add(arriba);	
+
+	private Translacion translacion;
+	// private LinkedList<Movimiento> movimientos;
+	// private Derecha derecha;
+	// private Izquierda izquierda;
+	// private Arriba arriba;
+	// private Abajo abajo;
+
+	public Movimiento() {
+		// this.movimientos = new LinkedList<Movimiento>();
+		this.translacion = new Translacion(true,false,false,false);
+
+		// derecha = new Derecha();
+		// izquierda = new Izquierda();
+		// abajo = new Abajo();
+		// arriba = new Arriba();
+
+		// Inicializo la lista de objetos movimientos
+		// this.movimientos.add(derecha);
+		// this.movimientos.add(izquierda);
+		// this.movimientos.add(abajo);
+		// this.movimientos.add(arriba);
 	}
-	
-	public int obtenerNumeroRandom(){
-		//Devuelve un numero entre 0 y 3.
-		int numero = (int) Math.round((Math.random()*1000)); 
-		return ( (numero % 3) + 1);
+
+	public int obtenerNumeroRandom() {
+		// Devuelve un numero entre 0 y 3.
+		int numero = (int) Math.round((Math.random() * 1000));
+		return ((numero % 3) + 1);
 	}
-	
 
 	public abstract void mover(Personaje unPersonaje);
 
