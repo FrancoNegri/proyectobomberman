429cf6c799ba5fa7e751f96efea75c37a1b06ca1
franconegri2004@gmail.com <franconegri2004@gmail.com@63393462-3f79-a54c-fcff-b23df89fc2d5>
2012-06-17 20:59:25 +0000

    eliminada la clase Translacion antigua que ya nadie usaba(gracias a fede que se dio cuenta)

--- trunk/BombitaTp2/test/modelo/coordenadastest/TranslacionTest.java
+++ trunk/BombitaTp2/test/modelo/coordenadastest/TranslacionTest.java
@@ -1,7 +1,8 @@
 package modelo.coordenadastest;
 
+import modelo.Translacion.Translacion;
+import modelo.Translacion.TranslacionAbajo;
 import modelo.coordenadas.Coordenada;
-import modelo.coordenadas.Translacion;
 import static org.mockito.Mockito.*;
 import org.junit.Test;
 
@@ -12,7 +13,7 @@ public class TranslacionTest {
 	Coordenada MiCoordenada = mock(Coordenada.class);
 	when(MiCoordenada.obtenerCoordenadaX()).thenReturn(2);
 	when(MiCoordenada.obtenerCoordenadaY()).thenReturn(2);
-	Translacion trans = new Translacion(true,false,false,false);
+	Translacion trans = new TranslacionAbajo();
 	trans.accion(MiCoordenada);
 	verify(MiCoordenada,atLeastOnce()).obtenerCoordenadaX();
 	}
