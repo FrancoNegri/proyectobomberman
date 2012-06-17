290826a8fcf9f8dab9d029ca7918b59c3f215286
fede.dirocco@gmail.com <fede.dirocco@gmail.com@63393462-3f79-a54c-fcff-b23df89fc2d5>
2012-06-17 19:33:37 +0000

    Subo los do sque faltaban, no los habia tildado

---
+++ trunk/BombitaTp2/test/modelo/enemigostest/CecilioTest.java
@@ -0,0 +1,74 @@
+package modelo.enemigostest;
+
+import static org.junit.Assert.*;
+import modelo.ArmamentoFactory.*;
+import modelo.coordenadas.Coordenada;
+import modelo.personaje.enemigos.*;
+import org.junit.*;
+
+public class CecilioTest {
+
+	private Cecilio lopez;
+	private Coordenada coordenadaC;
+	private ArmamentoFactory molotov;
+	
+	@Before
+	public void setUp(){
+		coordenadaC = new Coordenada(8,2);
+		lopez = new Cecilio(coordenadaC);
+		molotov = new MolotovFactory();
+	}
+	
+	@Test
+	public void testInicializarVelocidad() {
+		assertTrue(lopez.getVelocidad() == 100);
+	}
+	
+	@Test
+	public void testInicializarBomba() {
+		assertEquals(molotov.getClass(),lopez.tirarArmamento().getClass());
+	}
+	
+
+	@Test
+	public void testInicializarVida() {
+		assertTrue(lopez.obtenerVida() == 100);
+	}
+	
+	@Test
+	public void testInicializarCoordenadaX() {
+		assertTrue(lopez.obtenerCoordenadaXY().obtenerCoordenadaX() == 8);
+	}
+	
+	@Test
+	public void testInicializarCoordenadaY() {
+		assertTrue(lopez.obtenerCoordenadaXY().obtenerCoordenadaY() == 2);
+	}
+	
+	@Test
+	public void testRestarVida() {
+		lopez.restarVida(10);
+		assertTrue(lopez.obtenerVida() == 90);
+	}
+	
+	@Test
+	public void testSinVida() {
+		lopez.restarVida(100);
+		assertTrue(lopez.estaMuerto() == true);
+	}
+	
+	//Agregar test de caminar (los agrego cuando modifique la clase movimiento usando la de translacion)
+	
+	@Test
+	public void testDaniar(){
+		this.lopez.Daniar(20);
+		assertTrue(lopez.obtenerVida() == 80);
+	}
+	
+	@Test
+	public void testchocar(){
+		lopez.chocar();
+		assertTrue(lopez.obtenerVida() == 99);
+	}
+	
+}
