1b15a6c4dd49d337329b3c3522920e9dac84ec2a
franconegri2004@gmail.com <franconegri2004@gmail.com@63393462-3f79-a54c-fcff-b23df89fc2d5>
2012-06-22 21:23:10 +0000

    update

--- trunk/BombitaTp2/src/vista/Bloque/VistaBloque.java
+++ trunk/BombitaTp2/src/vista/Bloque/VistaBloque.java
@@ -6,8 +6,6 @@ import vista.fiuba.algo3.titiritero.dibujables.Cuadrado;
 import vista.fiuba.algo3.titiritero.modelo.ObjetoPosicionable;
 
 public class VistaBloque extends Cuadrado{
-
-	
 	public VistaBloque(ObjetoPosicionable obstaculoAMostrar){
 		super(2,4,obstaculoAMostrar);
 		this.setColor(Color.YELLOW);
