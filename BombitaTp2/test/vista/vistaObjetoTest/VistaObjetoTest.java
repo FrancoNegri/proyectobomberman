3d43e807efc72b30060ba1c2c66fd77dd5346dd0
franconegri2004@gmail.com <franconegri2004@gmail.com@63393462-3f79-a54c-fcff-b23df89fc2d5>
2012-06-23 18:20:42 +0000

    añadido test vistaObjeto

---
+++ BombitaTp2/test/vista/vistaObjetoTest/VistaObjetoTest.java
@@ -0,0 +1,160 @@
+package vista.vistaObjetoTest;
+
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.awt.Graphics;
+
+import modelo.casillero.Casillero;
+import modelo.coordenadas.Coordenada;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import vista.fiuba.algo3.titiritero.dibujables.SuperficiePanel;
+import vista.fiuba.algo3.titiritero.modelo.ObjetoDibujable;
+import vista.fiuba.algo3.titiritero.modelo.ObjetoPosicionable;
+import vista.objeto.ObjetoVisible;
+import vista.objeto.VistaObjeto;
+
+
+public class VistaObjetoTest {
+
+	
+	Graphics graphics = Mockito.mock(Graphics.class);
+	SuperficiePanel unaSuperficie = Mockito.mock(SuperficiePanel.class);
+	Casillero casilleroMock = mock(Casillero.class);
+	ObjetoPosicionable objeto = Mockito.mock(ObjetoPosicionable.class);
+	@Before
+	public void SetUp(){
+		Mockito.when(unaSuperficie.getBuffer()).thenReturn(graphics);
+		Mockito.when(objeto.getX()).thenReturn(5);
+		Mockito.when(objeto.getY()).thenReturn(5);
+
+	}
+	
+	@Test
+	public void testQuePruevaLaVistaDeCecilio(){
+		VistaObjeto vistaO = new VistaObjeto(mock(ObjetoVisible.class));
+		vistaO.interactuarConCecilio(objeto);
+		vistaO.dibujar(unaSuperficie);
+		//A Determinar
+		//Mockito.verify(graphics).setColor(circulo.getColor());
+		//Mockito.verify(graphics).fillOval(objeto.getX()*5, objeto.getY()*5, circulo.getRadio(), circulo.getRadio());
+	}
+	
+	@Test
+	public void testQuePruevaLaVistaDeBombita(){
+		VistaObjeto vistaO = new VistaObjeto(mock(ObjetoVisible.class));
+		vistaO.InteractuarConBombita(objeto);
+		vistaO.dibujar(unaSuperficie);
+		//A Determinar
+		//Mockito.verify(graphics).setColor(circulo.getColor());
+		//Mockito.verify(graphics).fillOval(objeto.getX()*5, objeto.getY()*5, circulo.getRadio(), circulo.getRadio());
+	}
+	
+	@Test
+	public void testQuePruevaLaVistaDeLopezComun(){
+		VistaObjeto vistaO = new VistaObjeto(mock(ObjetoVisible.class));
+		vistaO.InteractuarConLopezComun(objeto);
+		vistaO.dibujar(unaSuperficie);
+		//A Determinar
+		//Mockito.verify(graphics).setColor(circulo.getColor());
+		//Mockito.verify(graphics).fillOval(objeto.getX()*5, objeto.getY()*5, circulo.getRadio(), circulo.getRadio());
+	}
+	
+	@Test
+	public void testQuePruevaLaVistaDeLopezAlado(){
+		VistaObjeto vistaO = new VistaObjeto(mock(ObjetoVisible.class));
+		vistaO.InteractuarConLopezAlado(objeto);
+		vistaO.dibujar(unaSuperficie);
+		//A Determinar
+		//Mockito.verify(graphics).setColor(circulo.getColor());
+		//Mockito.verify(graphics).fillOval(objeto.getX()*5, objeto.getY()*5, circulo.getRadio(), circulo.getRadio());
+	}
+	
+	@Test
+	public void testQuePruevaLaVistaDelToleTole(){
+		VistaObjeto vistaO = new VistaObjeto(mock(ObjetoVisible.class));
+		vistaO.InteractuarConToleTole(objeto);
+		vistaO.dibujar(unaSuperficie);
+		//A Determinar
+		//Mockito.verify(graphics).setColor(circulo.getColor());
+		//Mockito.verify(graphics).fillOval(objeto.getX()*5, objeto.getY()*5, circulo.getRadio(), circulo.getRadio());
+	}
+	
+	@Test
+	public void testQuePruevaLaVistaDelProyectil(){
+		VistaObjeto vistaO = new VistaObjeto(mock(ObjetoVisible.class));
+		vistaO.InteractuarConProyectil(objeto);
+		vistaO.dibujar(unaSuperficie);
+		//A Determinar
+		//Mockito.verify(graphics).setColor(circulo.getColor());
+		//Mockito.verify(graphics).fillOval(objeto.getX()*5, objeto.getY()*5, circulo.getRadio(), circulo.getRadio());
+	}
+	
+	@Test
+	public void testQuePruevaLaVistaDelMolotov(){
+		VistaObjeto vistaO = new VistaObjeto(mock(ObjetoVisible.class));
+		vistaO.InteractuarConMolotov(objeto);
+		vistaO.dibujar(unaSuperficie);
+		//A Determinar
+		//Mockito.verify(graphics).setColor(circulo.getColor());
+		//Mockito.verify(graphics).fillOval(objeto.getX()*5, objeto.getY()*5, circulo.getRadio(), circulo.getRadio());
+	}
+	
+	@Test
+	public void testQuePruevaLaVistaDeHabano(){
+		VistaObjeto vistaO = new VistaObjeto(mock(ObjetoVisible.class));
+		vistaO.InteractuarConHabano(objeto);
+		vistaO.dibujar(unaSuperficie);
+		//A Determinar
+		//Mockito.verify(graphics).setColor(circulo.getColor());
+		//Mockito.verify(graphics).fillOval(objeto.getX()*5, objeto.getY()*5, circulo.getRadio(), circulo.getRadio());
+	}
+	
+	@Test
+	public void testQuePruevaLaVistaDelTimer(){
+		VistaObjeto vistaO = new VistaObjeto(mock(ObjetoVisible.class));
+		vistaO.InteractuarTimer(objeto);
+		vistaO.dibujar(unaSuperficie);
+		//A Determinar
+		//Mockito.verify(graphics).setColor(circulo.getColor());
+		//Mockito.verify(graphics).fillOval(objeto.getX()*5, objeto.getY()*5, circulo.getRadio(), circulo.getRadio());
+	}
+	
+	@Test
+	public void testQuePruevaLaVistaDelBloqueCemento(){
+		VistaObjeto vistaO = new VistaObjeto(mock(ObjetoVisible.class));
+		vistaO.InteractuarConBloqueCemento(objeto);
+		vistaO.dibujar(unaSuperficie);
+		//A Determinar
+		//Mockito.verify(graphics).setColor(circulo.getColor());
+		//Mockito.verify(graphics).fillOval(objeto.getX()*5, objeto.getY()*5, circulo.getRadio(), circulo.getRadio());
+	}
+	
+	@Test
+	public void testQuePruevaLaVistaDelBloqueAcero(){
+		VistaObjeto vistaO = new VistaObjeto(mock(ObjetoVisible.class));
+		vistaO.InteractuarConBloqueAcero(objeto);
+		vistaO.dibujar(unaSuperficie);
+		//A Determinar
+		//Mockito.verify(graphics).setColor(circulo.getColor());
+		//Mockito.verify(graphics).fillOval(objeto.getX()*5, objeto.getY()*5, circulo.getRadio(), circulo.getRadio());
+	}
+	
+	@Test
+	public void testQuePruevaLaVistaDelBloqueLadrillo(){
+		VistaObjeto vistaO = new VistaObjeto(mock(ObjetoVisible.class));
+		vistaO.InteractuarConBloqueLadrillo(objeto);
+		vistaO.dibujar(unaSuperficie);
+		//A Determinar
+		//Mockito.verify(graphics).setColor(circulo.getColor());
+		//Mockito.verify(graphics).fillOval(objeto.getX()*5, objeto.getY()*5, circulo.getRadio(), circulo.getRadio());
+	}
+	
+	
+	
+}
