e531c5a25a592e6e7d2db996805e519a80534be2
franconegri2004@gmail.com <franconegri2004@gmail.com@63393462-3f79-a54c-fcff-b23df89fc2d5>
2012-06-27 19:13:15 +0000

    otra modificacion, el juego tendria que andar sin trabarse tanto.

--- BombitaTp2/src/vista/Mapa/VistaMapa.java
+++ BombitaTp2/src/vista/Mapa/VistaMapa.java
@@ -8,19 +8,24 @@ import modelo.coordenadas.Coordenada;
 import modelo.mapa.Mapa;
 
 public class VistaMapa implements ObjetoDibujable {
-	
+
 	private Mapa mapaDibujable;
-	public VistaMapa(Mapa mapa){
+
+	public VistaMapa(Mapa mapa) {
 		mapaDibujable = mapa;
 	}
-	
+
 	public void dibujar(SuperficieDeDibujo superficieDeDibujo) {
-		for(int j =0 ;j < mapaDibujable.obtenerTamanio() ; j++){
-			for(int k =0 ;k < mapaDibujable.obtenerTamanio(); k++){
-				Coordenada coordenada = new Coordenada(j,k);
-				Casillero casillero = mapaDibujable.obtenerCasillero(coordenada);
-				VistaCasillero vistaDelCasillero = new VistaCasillero(casillero);
-				vistaDelCasillero.dibujar(superficieDeDibujo);
+		for (int j = 0; j < mapaDibujable.obtenerTamanio(); j++) {
+			for (int k = 0; k < mapaDibujable.obtenerTamanio(); k++) {
+				Coordenada coordenada = new Coordenada(j, k);
+				Casillero casillero = mapaDibujable
+						.obtenerCasillero(coordenada);
+				if (!casillero.casilleroEstaVacio()) {
+					VistaCasillero vistaDelCasillero = new VistaCasillero(
+							casillero);
+					vistaDelCasillero.dibujar(superficieDeDibujo);
+				}
 			}
 		}
 	}
