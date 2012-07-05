c422dae5e81f9d57e7c01b76ea897d2a04fa58f9
fede.dirocco@gmail.com <fede.dirocco@gmail.com@63393462-3f79-a54c-fcff-b23df89fc2d5>
2012-07-05 21:00:24 +0000

    Sopresaa para la IU, agrego un cargador de juego!

--- trunk/BombitaTp2/src/main/Main.java
+++ trunk/BombitaTp2/src/main/Main.java
@@ -3,10 +3,18 @@ package main;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 
+import modelo.errores.TiempoInvalidoError;
+
 	public class Main {
 		private static JFrame frame;
+		
 		public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
+			new VentanaCargador();
+			for (int vuelta=0; vuelta<6; vuelta++) {
+				for (float i=0; i<10000000; i++) i = (float) (i + 0.1);
+			}
+			EventQueue.invokeLater(new Runnable() {
+			
 			public void run() {
 				try {
 					VentanaPrincipal window = new VentanaPrincipal();
@@ -17,5 +25,5 @@ import javax.swing.JFrame;
 				}
 			}
 		});
-	}
+		}
 }
